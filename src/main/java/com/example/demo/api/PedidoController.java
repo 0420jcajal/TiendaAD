36f8eec056421617f95b32e1806ab89bf0f2ad3e
0420jcajal <0420jcajal@e-itaca.es>
2025-02-26 15:46:42 +0100

    2. Añadimos pedidos

---
+++ src/main/java/com/example/demo/api/PedidoController.java
@@ -0,0 +1,39 @@
+package com.example.demo.api;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.models.Pedido;
+import com.example.demo.services.PedidoService;
+import com.example.demo.requests.PedidoCreationRequest;
+
+
+
+@RestController
+@RequestMapping("/api/v1/pedidos")
+@CrossOrigin(origins="*")
+public class PedidoController {
+    private final PedidoService pedidoService;
+
+    public PedidoController(PedidoService pedidoService){
+        this.pedidoService= pedidoService;
+    }
+
+    @PostMapping
+    public Pedido createPedido(@RequestBody PedidoCreationRequest pedidoCreationRequest){
+        return pedidoService.creatPedido(pedidoCreationRequest);
+    }
+    @GetMapping("/{numeroPedido}")
+    public Pedido getPedido(@PathVariable long numeroPedido){
+        return pedidoService.getPedido(numeroPedido).orElse(null);
+    }
+    @DeleteMapping("/{numeroPedido}")
+    public void deletePedido(@PathVariable long numeroPedido){
+        pedidoService.removePedido(numeroPedido);
+    }
+    @GetMapping("/getall")
+    public List<Pedido> getAllPedidos(){
+        return pedidoService.getAllPedidos();
+    }
+}
--- src/main/java/com/example/demo/services/PedidoService.java
+++ src/main/java/com/example/demo/services/PedidoService.java
@@ -11,10 +11,49 @@ import com.example.demo.models.Pedido;
 import com.example.demo.repository.PedidoRepository;
 import com.example.demo.requests.PedidoCreationRequest;
 
+@Service
 public class PedidoService {
 
     private static final Logger logger= LoggerFactory.getLogger(PedidoService.class);
 
     private final PedidoRepository pedidoRepository;
+
+    public PedidoService(PedidoRepository pedidoRepository){
+        this.pedidoRepository= pedidoRepository;
+    }
+
+    public Pedido creatPedido(PedidoCreationRequest pedidoCreationRequest){
+        return pedidoRepository.save(mapToPedido(pedidoCreationRequest));
+    }
+    public void removePedido(Long numeroPedido){
+        pedidoRepository.deleteById(numeroPedido);
+    }
+    public Optional<Pedido> getPedido(final Long numeroPedido){
+        try {
+            return pedidoRepository.findById(numeroPedido);
+        } catch (Exception e) {
+            logger.error("Error al recuperar el pedido con numero {}. Excepcion: {}",numeroPedido, e);
+            return null;
+        }
+    }
+    public List<Pedido> getAllPedidos(){
+        try {
+            return pedidoRepository.findAll();
+        } catch (Exception e) {
+            logger.error("Error al recuperar la lista de pedidos. Excepcion: {}", e);
+            return null;
+        }
+
+    }
     
+
+    public Pedido mapToPedido(PedidoCreationRequest pedidoCreationRequest){
+        Pedido pedido= new Pedido();
+        pedido.setNumeroPedido(pedidoCreationRequest.numeroPedido());
+        pedido.setDescripcion(pedidoCreationRequest.descripcion());
+        pedido.setPrecio(pedidoCreationRequest.precio());
+        pedido.setEstado(pedidoCreationRequest.estado());
+        pedido.setComprador(pedidoCreationRequest.comprador());
+        return pedido;
+    }
 }
